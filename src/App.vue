538ee88eace5cbf858df66757e29997b23bbc0db
shaoual <127392211+shaoual@users.noreply.github.com>
2024-04-11 15:46:06 +0200

    ajout bouton

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,21 @@ onErrorCaptured((err, instance, info) => {
 
 <template>
   <header>
+    <button aria-controls="mainNav" aria-expanded="true" class="rounded-full border-2 border-red-600 bg-red-300 px-2">
+      menu
+    </button>
+    <!-- nav#mainNav>ul>li*3>a[href="#"]{item $} -->
+    <nav id="mainNav">
+      <ul>
+        <li><a href="#">item 1</a></li>
+        <li><a href="#">item 2</a></li>
+        <li><a href="#">item 3</a></li>
+      </ul>
+    </nav>
+
     <nav>
       <ul>
-        <li> 
+        <li>
           <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
         </li>
       </ul>
