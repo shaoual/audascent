1f794875774895ab54747d66661692b814da013d
orly-chanella <orly_chanella.irumva@edu.univ-fcomte.fr>
2024-05-27 14:59:59 +0200

    ff

---
+++ pocketbase1/pb_migrations/1716811977_updated_classement.js
@@ -0,0 +1,168 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("fp63glgi5wnl33i")
+
+  // remove
+  collection.schema.removeField("80cz9zhy")
+
+  // remove
+  collection.schema.removeField("olcpmqzs")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "qreeoia5",
+    "name": "Number",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "w0vgthwh",
+    "name": "Profil",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ehtbkp5j",
+    "name": "NomJoueurs",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "qofie2gn",
+    "name": "Pays",
+    "type": "file",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "mimeTypes": [],
+      "thumbs": [],
+      "maxSelect": 1,
+      "maxSize": 5242880,
+      "protected": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "ifttl53w",
+    "name": "Score",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "0jdpikiw",
+    "name": "Progression",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("fp63glgi5wnl33i")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "80cz9zhy",
+    "name": "nom",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "olcpmqzs",
+    "name": "score",
+    "type": "number",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "noDecimal": false
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("qreeoia5")
+
+  // remove
+  collection.schema.removeField("w0vgthwh")
+
+  // remove
+  collection.schema.removeField("ehtbkp5j")
+
+  // remove
+  collection.schema.removeField("qofie2gn")
+
+  // remove
+  collection.schema.removeField("ifttl53w")
+
+  // remove
+  collection.schema.removeField("0jdpikiw")
+
+  return dao.saveCollection(collection)
+})
---
+++ pocketbase1/pb_migrations/1716812592_created_Tournois.js
@@ -0,0 +1,139 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const collection = new Collection({
+    "id": "6vxvzpvdov1ir22",
+    "created": "2024-05-27 12:23:12.754Z",
+    "updated": "2024-05-27 12:23:12.754Z",
+    "name": "Tournois",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "cfiqaldt",
+        "name": "Nom",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "vuz1lil0",
+        "name": "Description",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "w5smowxv",
+        "name": "Date_heure_de_debut",
+        "type": "date",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "lhzj2y40",
+        "name": "Date_heure_de_fin",
+        "type": "date",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "803equst",
+        "name": "Jeu_du_tournoi",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "sbru7rye",
+        "name": "img_jeu",
+        "type": "file",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "mimeTypes": [],
+          "thumbs": [],
+          "maxSelect": 1,
+          "maxSize": 5242880,
+          "protected": false
+        }
+      },
+      {
+        "system": false,
+        "id": "qylrqwcs",
+        "name": "Nombre_max_des_participants",
+        "type": "number",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "noDecimal": false
+        }
+      },
+      {
+        "system": false,
+        "id": "yn5rvdof",
+        "name": "Etat_du_tournois",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],
+    "indexes": [],
+    "listRule": null,
+    "viewRule": null,
+    "createRule": null,
+    "updateRule": null,
+    "deleteRule": null,
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("6vxvzpvdov1ir22");
+
+  return dao.deleteCollection(collection);
+})
