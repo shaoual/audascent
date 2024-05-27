10a092a9aebbf9e4ef708d489e401490aa7656d9
shaoual <127392211+shaoual@users.noreply.github.com>
2024-05-27 14:54:01 +0200

    ajout page Apropos

---
+++ src/pages/apropos.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
+
+</script>
+
+
+<template> 
+    <main class="main-content">
+
+<h2>À PROPOS</h2>
+
+<div class="lg:w-1/2 mb-20">
+<p>Bienvenue sur Ascenita, la plateforme dédiée à l'univers des jeux vidéo et des compétitions en ligne.</p>
+
+<p class="mt-10">Le monde du gaming est vaste et en perpétuelle évolution, touchant des millions de joueurs autour du globe dans divers genres et plateformes. Que ce soit pour le divertissement, la compétition, ou le social, les jeux vidéo occupent une place centrale dans la culture numérique actuelle. Néanmoins, trouver un espace qui rassemble communauté, compétition et apprentissage peut s'avérer complexe.</p>
+
+<p class="mt-10">C'est dans cette optique qu'Ascenita a été créée. Notre plateforme offre un point de rencontre pour les joueurs de tous niveaux désireux de participer à des tournois, d'améliorer leurs compétences ou simplement de partager leur passion pour le gaming. Chez Ascenita, nous croyons en la force de la communauté et en l'importance de fournir un espace sécurisé et stimulant pour tous.
+</p>
+
+<p class="mt-10">Vous découvrirez sur Ascenita un calendrier complet des tournois à venir, accessibles à différents niveaux de compétence et sur une variété de jeux populaires. Chaque événement est conçu pour offrir une expérience compétitive équitable et excitante, avec des récompenses à la clé pour les plus méritants. Que vous soyez un joueur solo cherchant à vous mesurer aux autres ou que vous préfériez les affrontements en équipe, vous trouverez votre place sur Ascenita.</p>
+<p class="mt-10">Notre mission va au-delà de l'organisation de tournois. Nous nous engageons à créer un environnement où chaque joueur, qu'il soit débutant ou vétéran, peut apprendre, s'améliorer et, surtout, s'amuser. Ascenita est également un espace pour découvrir les dernières nouveautés du monde du gaming, partager des astuces et des stratégies, et se connecter avec d'autres joueurs partageant les mêmes intérêts.</p>
+<p class="mt-10">Nous vous invitons à explorer notre site, à participer à nos tournois, et à devenir membre de notre communauté grandissante. Si vous avez des questions, des suggestions, ou si vous souhaitez contribuer à Ascenita, n'hésitez pas à nous contacter. Merci de choisir Ascenita, votre plateforme de référence pour le gaming et les compétitions en ligne.</p>
+
+</div>
+
+    </main>
+    <RouterView />
+</template>
+
+
