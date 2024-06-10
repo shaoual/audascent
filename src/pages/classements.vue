eb4d932e2c38ca61d3bc4ef83852f6d394dcf48b
shaoual <127392211+shaoual@users.noreply.github.com>
2024-06-10 09:14:09 +0200

    ajout page + images

---
+++ src/pages/classements.vue
@@ -0,0 +1,15 @@
+   <script setup lang="ts">
+</script>
+
+
+
+
+<template>
+   <div class="relative h-96 bg-cover bg-center" style="background-image: url('/img/');">
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+    <div class="relative z-10 flex items-center justify-center h-full">
+      <h1 class="text-5xl font-bold text-white">Modernist</h1>
+    </div>
+  </div>
+
+  </template>
