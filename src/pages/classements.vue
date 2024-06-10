62aff0fe4aa64a2ca7fbcd01d2c7f0b9f849dfb4
shaoual <127392211+shaoual@users.noreply.github.com>
2024-06-10 10:34:18 +0200

    ajout texte page classements

--- src/pages/classements.vue
+++ src/pages/classements.vue
@@ -14,16 +14,44 @@
 
  <div class= "md:text-left md:col-span-2 ml-12 mt-12">
         <h3 class="text-2xl">Classement des Meilleurs Joueurs</h3> 
-        <p class="md:text-sm"> Découvrez les maîtres du jeu qui dominent les classements d'Ascenita. Ces champions ont su démontrer habileté, stratégie et persévérance pour atteindre le sommet. Consultez le tableau ci-dessous pour voir qui sont les étoiles de notre communauté e-sportive :
+        <p class="md:text-sm lg:w-1/2"> Découvrez les maîtres du jeu qui dominent les classements d'Ascenita. Ces champions ont su démontrer habileté, stratégie et persévérance pour atteindre le sommet. Consultez le tableau ci-dessous pour voir qui sont les étoiles de notre communauté e-sportive :
         </p>
+      
 
 
 
 <!--TABLEAU CLASSEMENTS-->
 
         <h3 class="text-2xl mt-12">Spotlight sur les Tournois</h3> 
-        <img src="/public/img/img2_classement.webp" class="h-auto rounded-lg lg:w-1/2 mt-10 mx-8" alt=".." />
+        <img src="/public/img/img2_classement.webp" class="h-auto rounded-lg lg:w-1/2 mt-10 mx-9" alt=".." />
+
+        
+         
+            <p class="mt-10 md:text-sm lg:w-1/2">
+      Ne ratez pas l'opportunité de vous mesurer aux meilleurs et d'inscrire votre nom dans l'histoire d'Ascentia. Voici les tournois actuellement en vedette, prêts à accueillir de nouveaux challengers :
+    </p>
+    <ul class="space-y-6">
+      <li>
+        <h2 class="text-2xl font-bold mb-2">Ascentia Global Masters :</h2>
+        <p class="lg:w-1/2">Le sommet de la compétition mondiale. Quelques places sont encore disponibles pour les audacieux prêts à affronter l'élite.</p>
+      </li>
+      <li>
+        <h2 class="text-2xl font-bold mb-2">League of Legends Ascentia Cup :</h2>
+        <p class="lg:w-1/2">Affûtez vos compétences et rejoignez la bataille pour la suprématie dans l'arène de LoL. Inscriptions ouvertes pour les équipes et les joueurs solo.</p>
+      </li>
+      <li>
+        <h2 class="text-2xl font-bold mb-2">Rocket League Ascentia Championship :</h2>
+        <p class="lg:w-1/2">Faites rugir vos moteurs et lancez-vous dans une compétition effrénée. Places limitées, ne manquez pas votre chance de participer.</p>
+      </li>
+    </ul>
+
+
+      <h3 class="text-2xl mt-24 ">Classement des Meilleurs Joueurs</h3> 
+        <p class="md:text-sm  mx-0."> Découvrez les maîtres du jeu qui dominent les classements d'Ascenita. Ces champions ont su démontrer habileté, stratégie et persévérance pour atteindre le sommet. Consultez le tableau ci-dessous pour voir qui sont les étoiles de notre communauté e-sportive :
+        </p>
+  </div>
+
+    
        
-    </div>
   
   </template>
