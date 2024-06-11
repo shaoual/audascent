d3b1f7a84eaa23e4be19fcd8eb8b559f2c88f690
shaoual <127392211+shaoual@users.noreply.github.com>
2024-06-11 10:56:59 +0200

    ajout fonction + ajout import dans typegen

--- src/pocketbase-types.ts
+++ src/pocketbase-types.ts
@@ -1,10 +1,72 @@
 /**
- * Générer ce fichier avec la commande suivante:
- * npm run typegen
- */
+* This file was @generated using pocketbase-typegen
+*/
 
 import type PocketBase from 'pocketbase'
+import type { RecordService } from 'pocketbase'
+
+export enum Collections {
+	Jeux = "jeux",
+	Users = "users",
+}
+
+// Alias types for improved usability
+export type IsoDateString = string
+export type RecordIdString = string
+export type HTMLString = string
+
+// System fields
+export type BaseSystemFields<T = never> = {
+	id: RecordIdString
+	created: IsoDateString
+	updated: IsoDateString
+	collectionId: string
+	collectionName: Collections
+	expand?: T
+}
+
+export type AuthSystemFields<T = never> = {
+	email: string
+	emailVisibility: boolean
+	username: string
+	verified: boolean
+} & BaseSystemFields<T>
+
+// Record types for each collection
+
+export type JeuxRecord = {
+	AnneDeCreation?: number
+	Nom?: string
+	biographie?: string
+	createur?: string
+	genre?: string
+}
+
+export type UsersRecord = {
+	avatar?: string
+	name?: string
+}
+
+// Response types include system fields and match responses from the PocketBase API
+export type JeuxResponse<Texpand = unknown> = Required<JeuxRecord> & BaseSystemFields<Texpand>
+export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
+
+// Types containing all Records and Responses, useful for creating typing helper functions
+
+export type CollectionRecords = {
+	jeux: JeuxRecord
+	users: UsersRecord
+}
+
+export type CollectionResponses = {
+	jeux: JeuxResponse
+	users: UsersResponse
+}
+
 // Type for usage with type asserted PocketBase instance
 // https://github.com/pocketbase/js-sdk#specify-typescript-definitions
 
-export type TypedPocketBase = PocketBase & {}
+export type TypedPocketBase = PocketBase & {
+	collection(idOrName: 'jeux'): RecordService<JeuxResponse>
+	collection(idOrName: 'users'): RecordService<UsersResponse>
+}
